e32a0c9684ff612fcb5ae60df91cc0d42c445461
Xiwei Pan <xiwei.pan@connect.hkust-gz.edu.cn>
2026-09-08 16:43:47 +0800

    Add failing Lennard-Jones unit tests

--- week2/md/src/lib.rs
+++ week2/md/src/lib.rs
@@ -10,4 +10,14 @@ mod tests {
     fn greeting_is_hello_world() {
         assert_eq!(greeting(), "Hello, world!");
     }
+
+    #[test]
+    fn lennard_jones_energy_at_two() {
+        assert_eq!(super::lennard_jones_energy(2.0), -0.0615234375);
+    }
+
+    #[test]
+    fn lennard_jones_force_at_two() {
+        assert_eq!(super::lennard_jones_force(2.0), -0.181640625);
+    }
 }
